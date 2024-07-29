bf33dff29c87a3538972117bb41cd38ee74f478c
Robert Solorzano <robertsolorzano510@gmail.com>
2024-07-28 21:41:09 -0700

    debiggin latency endoint

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
+// const express = require('express');
+// const app = express();
+// const port = process.env.PORT || 3000;
+
+// // Import all routes from routes/index.js
+// const routes = require('./routes');
+
+// // Use routes
+// app.use(routes);
+
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}/`);
+// });
+
+
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Import all routes from routes/index.js
-const routes = require('./routes');
+// Middleware to set startTimestamp
+app.use((req, res, next) => {
+    req.startTimestamp = Date.now();
+    next();
+});
 
-// Use routes
-app.use(routes);
+const routes = require('./routes');
+app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
