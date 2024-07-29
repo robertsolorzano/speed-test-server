4433b2bdcacd58e45d6c0289681afddc79d9bcfc
Robert Solorzano <robertsolorzano510@gmail.com>
2024-07-28 21:31:56 -0700

    fixed

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware to set startTimestamp
-app.use((req, res, next) => {
-    req.startTimestamp = Date.now();
-    next();
-});
-
+// Import all routes from routes/index.js
 const routes = require('./routes');
-app.use('/api', routes);
+
+// Use routes
+app.use(routes);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
