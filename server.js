0c316350b123402f0ef218feb494f678ab960c7e
Robert Solorzano <robertsolorzano510@gmail.com>
2024-07-28 21:22:14 -0700

    removed middleware, debugging to see if causing network error

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Import all routes from routes/index.js
 const routes = require('./routes');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.use((req, res, next) => {
-    req.startTimestamp = Date.now();
-    next();
-});
-
 // Use routes
-app.use('/api', routes);
+app.use(routes);
 
-// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
