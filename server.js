0af0407bd6013629574a0772b335c0fe3cf05f50
Robert Solorzano <robertsolorzano510@gmail.com>
2024-07-28 21:14:30 -0700

    Update server configuration

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const routes = require('./routes');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     req.startTimestamp = Date.now();
     next();
 });
 
-const routes = require('./routes');
-
 // Use routes
-app.use(routes);
+app.use('/api', routes);
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
