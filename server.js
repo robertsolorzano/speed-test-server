0d2185e8ef0bc2b6eec8917bbe08ef6f396677b7
Robert Solorzano <robertsolorzano510@gmail.com>
2024-07-28 21:29:37 -0700

    added back middleware for time only to measure latency server process time

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Import all routes from routes/index.js
-const routes = require('./routes');
+// Middleware to set startTimestamp
+app.use((req, res, next) => {
+    req.startTimestamp = Date.now();
+    next();
+});
 
-// Use routes
-app.use(routes);
+const routes = require('./routes');
+app.use('/api', routes);
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
