68a2895ca8875164d4d8d7c21672282173d23e25
Robert Solorzano <robertsolorzano510@gmail.com>
2024-07-28 21:38:38 -0700

    debiggin latency endoint

--- routes/latency.js
+++ routes/latency.js
@@ -1,12 +1,30 @@
+// const express = require('express');
+// const router = express.Router();
+
+// router.get('/', (req, res) => {
+//     const serverTimestamp = Date.now();
+//     res.json({
+//         message: 'Latency test',
+//         serverTimestamp: serverTimestamp,
+//         serverProcessingTime: serverTimestamp - req.startTimestamp 
+//     });
+// });
+
+// module.exports = router;
+
+
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     const serverTimestamp = Date.now();
+    const serverProcessingTime = serverTimestamp - req.startTimestamp;
+    console.log(`Request startTimestamp: ${req.startTimestamp}`);
+    console.log(`Server Processing Time: ${serverProcessingTime} ms`);
     res.json({
         message: 'Latency test',
         serverTimestamp: serverTimestamp,
-        serverProcessingTime: serverTimestamp - req.startTimestamp 
+        serverProcessingTime: serverProcessingTime
     });
 });
 
